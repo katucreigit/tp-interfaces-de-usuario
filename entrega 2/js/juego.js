a6a40f11e542ed1c30628587bfe090e765a09964
Katu Crei <katucrei.git@gmail.com>
2026-09-23 02:30:02 -0300

    modals

--- entrega 2/js/juego.js
+++ entrega 2/js/juego.js
@@ -30,13 +30,66 @@ const intervalo = setInterval(() => {
 }, 1000);
 
 
-const favoritos = document.querySelectorAll(".favorito");
+    
+fetch('https://vj.interfaces.jima.com.ar/api/v2')
+    .then(response => response.json())
+    .then(juegos => {
 
-favoritos.forEach(boton => {
-    boton.addEventListener("click", () => {
-        const corazon = boton.querySelector("i");
+        const contenedor = document.getElementById('contenedor-juegos');
 
-        corazon.classList.toggle("fa-regular");
-        corazon.classList.toggle("fa-solid");
+        juegos.forEach(juego => {
+
+            const card = document.createElement('div');
+            card.classList.add('card-juego');
+
+            card.innerHTML = `
+                <img src="${juego.background_image_low_res}" alt="${juego.name}">
+
+                <div class="overlay">
+                    <h3>${juego.name}</h3>
+
+                    <button class="btn-jugar">Jugar</button>
+
+                    <button class="fav tooltip-acento"
+                            data-tooltip="Añadir a favoritos">
+                        <span>Añadir a favoritos</span>
+                        <i class="fa-regular fa-heart"></i>
+                    </button>
+                </div>
+            `;
+
+            contenedor.appendChild(card);
+
+            const botonFavorito = card.querySelector(".favorito");
+
+            botonFavorito.addEventListener("click", function() {
+                const corazon = this.querySelector("i");
+
+                corazon.classList.toggle("fa-regular");
+                corazon.classList.toggle("fa-solid");
+            });
+
+        });
+
+    })
+    .catch(error => {
+        console.error('Error al obtener los juegos:', error);
     });
+    
+const botonAjustes = document.getElementById("boton-ajustes");
+const menuAjustes = document.querySelector(".menu-ajustes");
+
+botonAjustes.addEventListener("click", function(event) {
+    event.stopPropagation();
+
+    menuAjustes.classList.toggle("abierto");
 });
+
+menuAjustes.addEventListener("click", function(event) {
+    event.stopPropagation();
+});
+
+document.addEventListener("click", function() {
+    menuAjustes.classList.remove("abierto");
+});
+
